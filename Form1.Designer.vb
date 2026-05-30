2b6027e49c330e82634641ebc8a65f7667c313ca
Naomi Celeres <naomiceleres21@gmail.com>
2026-05-30 15:23:06 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmElevator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.pnlShaft = New System.Windows.Forms.Panel()
+        Me.pnlCabin = New System.Windows.Forms.Panel()
+        Me.pnlRightDoor = New System.Windows.Forms.Panel()
+        Me.pnlLeftDoor = New System.Windows.Forms.Panel()
+        Me.btnFloor1 = New System.Windows.Forms.Button()
+        Me.btnFloor2 = New System.Windows.Forms.Button()
+        Me.btnFloor3 = New System.Windows.Forms.Button()
+        Me.lblFloor = New System.Windows.Forms.Label()
+        Me.tmrMove = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrDoor = New System.Windows.Forms.Timer(Me.components)
+        Me.btnFloor4 = New System.Windows.Forms.Button()
+        Me.pnlShaft.SuspendLayout()
+        Me.pnlCabin.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlShaft
+        '
+        Me.pnlShaft.BackColor = System.Drawing.Color.Maroon
+        Me.pnlShaft.Controls.Add(Me.pnlCabin)
+        Me.pnlShaft.Location = New System.Drawing.Point(292, 27)
+        Me.pnlShaft.Name = "pnlShaft"
+        Me.pnlShaft.Size = New System.Drawing.Size(144, 518)
+        Me.pnlShaft.TabIndex = 0
+        '
+        'pnlCabin
+        '
+        Me.pnlCabin.BackColor = System.Drawing.Color.Silver
+        Me.pnlCabin.Controls.Add(Me.pnlRightDoor)
+        Me.pnlCabin.Controls.Add(Me.pnlLeftDoor)
+        Me.pnlCabin.Location = New System.Drawing.Point(12, 402)
+        Me.pnlCabin.Name = "pnlCabin"
+        Me.pnlCabin.Size = New System.Drawing.Size(120, 80)
+        Me.pnlCabin.TabIndex = 0
+        '
+        'pnlRightDoor
+        '
+        Me.pnlRightDoor.AccessibleRole = System.Windows.Forms.AccessibleRole.None
+        Me.pnlRightDoor.BackColor = System.Drawing.Color.DeepPink
+        Me.pnlRightDoor.Location = New System.Drawing.Point(60, 0)
+        Me.pnlRightDoor.Name = "pnlRightDoor"
+        Me.pnlRightDoor.Size = New System.Drawing.Size(60, 80)
+        Me.pnlRightDoor.TabIndex = 1
+        '
+        'pnlLeftDoor
+        '
+        Me.pnlLeftDoor.BackColor = System.Drawing.Color.DeepPink
+        Me.pnlLeftDoor.Location = New System.Drawing.Point(0, 0)
+        Me.pnlLeftDoor.Name = "pnlLeftDoor"
+        Me.pnlLeftDoor.Size = New System.Drawing.Size(60, 80)
+        Me.pnlLeftDoor.TabIndex = 0
+        '
+        'btnFloor1
+        '
+        Me.btnFloor1.BackColor = System.Drawing.Color.Blue
+        Me.btnFloor1.Location = New System.Drawing.Point(575, 429)
+        Me.btnFloor1.Name = "btnFloor1"
+        Me.btnFloor1.Size = New System.Drawing.Size(75, 23)
+        Me.btnFloor1.TabIndex = 1
+        Me.btnFloor1.Text = "Floor 1"
+        Me.btnFloor1.UseVisualStyleBackColor = False
+        '
+        'btnFloor2
+        '
+        Me.btnFloor2.BackColor = System.Drawing.Color.Green
+        Me.btnFloor2.Location = New System.Drawing.Point(575, 356)
+        Me.btnFloor2.Name = "btnFloor2"
+        Me.btnFloor2.Size = New System.Drawing.Size(75, 23)
+        Me.btnFloor2.TabIndex = 2
+        Me.btnFloor2.Text = "Floor 2"
+        Me.btnFloor2.UseVisualStyleBackColor = False
+        '
+        'btnFloor3
+        '
+        Me.btnFloor3.BackColor = System.Drawing.Color.Goldenrod
+        Me.btnFloor3.Location = New System.Drawing.Point(575, 279)
+        Me.btnFloor3.Name = "btnFloor3"
+        Me.btnFloor3.Size = New System.Drawing.Size(75, 23)
+        Me.btnFloor3.TabIndex = 3
+        Me.btnFloor3.Text = "Floor 3"
+        Me.btnFloor3.UseVisualStyleBackColor = False
+        '
+        'lblFloor
+        '
+        Me.lblFloor.AutoSize = True
+        Me.lblFloor.BackColor = System.Drawing.Color.Pink
+        Me.lblFloor.Location = New System.Drawing.Point(586, 132)
+        Me.lblFloor.Name = "lblFloor"
+        Me.lblFloor.Size = New System.Drawing.Size(48, 16)
+        Me.lblFloor.TabIndex = 4
+        Me.lblFloor.Text = "Ready"
+        '
+        'tmrMove
+        '
+        Me.tmrMove.Interval = 20
+        '
+        'tmrDoor
+        '
+        Me.tmrDoor.Interval = 20
+        '
+        'btnFloor4
+        '
+        Me.btnFloor4.BackColor = System.Drawing.Color.Yellow
+        Me.btnFloor4.Location = New System.Drawing.Point(575, 203)
+        Me.btnFloor4.Name = "btnFloor4"
+        Me.btnFloor4.Size = New System.Drawing.Size(75, 23)
+        Me.btnFloor4.TabIndex = 5
+        Me.btnFloor4.Text = "Floor 4"
+        Me.btnFloor4.UseVisualStyleBackColor = False
+        '
+        'frmElevator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(782, 553)
+        Me.Controls.Add(Me.btnFloor4)
+        Me.Controls.Add(Me.lblFloor)
+        Me.Controls.Add(Me.btnFloor3)
+        Me.Controls.Add(Me.btnFloor2)
+        Me.Controls.Add(Me.btnFloor1)
+        Me.Controls.Add(Me.pnlShaft)
+        Me.Name = "frmElevator"
+        Me.Text = "Naomi Elevator Simulation"
+        Me.pnlShaft.ResumeLayout(False)
+        Me.pnlCabin.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents pnlShaft As Panel
+    Friend WithEvents pnlCabin As Panel
+    Friend WithEvents pnlLeftDoor As Panel
+    Friend WithEvents pnlRightDoor As Panel
+    Friend WithEvents btnFloor1 As Button
+    Friend WithEvents btnFloor2 As Button
+    Friend WithEvents btnFloor3 As Button
+    Friend WithEvents lblFloor As Label
+    Friend WithEvents tmrMove As Timer
+    Friend WithEvents tmrDoor As Timer
+    Friend WithEvents btnFloor4 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,141 @@
+﻿Public Class frmElevator
+    ' Variables for Movement
+    Dim targetTop As Integer
+    Dim moveSpeed As Integer = 3
+    Dim floor1 As Integer
+    Dim floor2 As Integer
+    Dim floor3 As Integer
+    Dim floor4 As Integer
+    Dim totalHeight As Integer
+    Dim stepHeight As Integer
+    Dim currentFloor As Integer = 1
+
+
+    ' Variables for Doors
+    Dim doorSpeed As Integer = 5
+    Dim doorState As String = "Closed"
+    Dim pendingFloor As Integer = 0
+
+    ' Door positions
+    Dim originalDoorLeft As Integer
+    Dim doorGap As Integer = 60
+
+    Private Sub frmElevator_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        totalHeight = pnlShaft.Height - pnlCabin.Height
+        stepHeight = totalHeight \ 3
+        floor1 = totalHeight - 10
+        floor2 = totalHeight - stepHeight
+        floor3 = totalHeight - (stepHeight * 2)
+        floor4 = 10
+
+
+    End Sub
+
+    ' --- BUTTONS ---
+
+    Private Sub btnFloor1_Click(sender As Object, e As EventArgs) Handles btnFloor1.Click
+        Call MoveToFloor(1, floor1)
+    End Sub
+
+    Private Sub btnFloor2_Click(sender As Object, e As EventArgs) Handles btnFloor2.Click
+        Call MoveToFloor(2, floor2)
+    End Sub
+
+    Private Sub btnFloor3_Click(sender As Object, e As EventArgs) Handles btnFloor3.Click
+        Call MoveToFloor(3, floor3)
+    End Sub
+
+    Private Sub MoveToFloor(floorNum As Integer, topPos As Integer)
+        lblFloor.Text = "Going to Floor " & floorNum
+        targetTop = topPos
+
+        If doorState <> "Closed" Then
+            pendingFloor = floorNum
+            doorState = "Closing"
+            tmrDoor.Start()
+        Else
+            pendingFloor = 0
+            tmrMove.Start()
+        End If
+    End Sub
+
+    ' --- MOVEMENT TIMER ---
+
+    Private Sub tmrMove_Tick(sender As Object, e As EventArgs) Handles tmrMove.Tick
+        If Math.Abs(pnlCabin.Top - targetTop) <= 5 Then
+            pnlCabin.Top = targetTop
+            tmrMove.Stop()
+            currentFloor = GetFloorNumber(targetTop)
+            lblFloor.Text = "CURRENT FLOOR: " & currentFloor
+            doorState = "Opening"
+            tmrDoor.Start()
+        ElseIf pnlCabin.Top > targetTop Then
+            pnlCabin.Top -= moveSpeed
+        Else
+            pnlCabin.Top += moveSpeed
+        End If
+    End Sub
+
+    ' --- DOOR TIMER ---
+
+    Private Sub tmrDoor_Tick(sender As Object, e As EventArgs) Handles tmrDoor.Tick
+
+        If doorState = "Opening" Then
+            If pnlLeftDoor.Left > originalDoorLeft - doorGap Then
+                pnlLeftDoor.Left -= doorSpeed
+                pnlRightDoor.Left += doorSpeed
+            Else
+                tmrDoor.Stop()
+                doorState = "Open"
+            End If
+
+        ElseIf doorState = "Closing" Then
+            If pnlLeftDoor.Left < originalDoorLeft Then
+                pnlLeftDoor.Left += doorSpeed
+                pnlRightDoor.Left -= doorSpeed
+            Else
+                tmrDoor.Stop()
+                doorState = "Closed"
+
+                Select Case pendingFloor
+                    Case 1
+                        targetTop = floor1
+                    Case 2
+                        targetTop = floor2
+                    Case 3
+                        targetTop = floor3
+                    Case 4
+                        targetTop = floor4
+
+                End Select
+                pendingFloor = 0
+                    tmrMove.Start()
+                End If
+            End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub pnlShaft_Paint(sender As Object, e As PaintEventArgs) Handles pnlShaft.Paint
+
+    End Sub
+    Private Sub btnFloor4_Click(sender As Object, e As EventArgs) Handles btnFloor4.Click
+        Call MoveToFloor(4, floor4)
+    End Sub
+
+
+
+
+    Private Function GetFloorNumber(pos As Integer) As Integer
+        If pos = floor1 Then Return 1
+        If pos = floor2 Then Return 2
+        If pos = floor3 Then Return 3
+        If pos = floor4 Then Return 4
+        Return 0
+    End Function
+
+End Class
+
